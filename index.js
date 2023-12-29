40d806ef6aaa04e8c95bd0aae7af65ec7a470911
Aryan Yadav <123636515+AryanYadav-git@users.noreply.github.com>
2023-12-29 15:49:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -33,6 +33,19 @@ const Admin = mongoose.model('Admin', adminSchema);
 const Course = mongoose.model('Course', courseSchema);
 
 const AuthenticatJwt = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+  if (authHeader) {
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, SECRET, (err, user) => {
+      if (err) {
+        return res.sendStatus(403);
+      }
+      req.user = user;
+      next();
+    });
+  } else {
+    res.sendStatus(401);
+  }
 
 }
 
